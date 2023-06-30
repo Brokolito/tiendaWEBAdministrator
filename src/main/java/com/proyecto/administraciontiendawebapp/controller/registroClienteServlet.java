811aaaf620fa3a6f14a8ba0a586aae2e288efeaf
Brokolito <m.castro14@ufromail.cl>
2023-06-30 16:43:33 -0400

    Se sube log out, login y otras paginas

--- src/main/java/com/proyecto/administraciontiendawebapp/controller/registroClienteServlet.java
+++ src/main/java/com/proyecto/administraciontiendawebapp/controller/registroClienteServlet.java
@@ -1,5 +1,7 @@
 package com.proyecto.administraciontiendawebapp.controller;
 
+import com.proyecto.administraciontiendawebapp.model.Cliente;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,7 +12,28 @@ import java.io.IOException;
 
 @WebServlet(name = "registroClienteServlet",value ="/registroCliente")
 public class registroClienteServlet extends HttpServlet {
-    public void doGet(HttpServletResponse response, HttpServletRequest request) throws IOException, ServletException {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher=request.getRequestDispatcher("/registroCliente.jsp");
+        System.out.println("registro");
+        requestDispatcher.forward(request,response);
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+        String rut=request.getParameter("rut");
+        String nombre=request.getParameter("nombre");
+        String direccion=request.getParameter("direccion");
+        String telefono=request.getParameter("telefono");
+        String correo=request.getParameter("correo");
+        RequestDispatcher requestDispatcher=request.getRequestDispatcher("/registroCliente.jsp");
+        if(!rut.isEmpty() && !nombre.isEmpty() && !direccion.isEmpty() && !telefono.isEmpty() && !correo.isEmpty()){
+            Cliente cliente=new Cliente(rut,nombre,direccion,telefono,correo);
+            if(cliente.registrarCliente()){
+                request.setAttribute("status","Se ha registrado existosamente el nuevo cliente");
+            }else{
+                request.setAttribute("status","El rut ya ha sido registrado");
+            }
+        }else{
+            request.setAttribute("status","Complete correctamente todos los campos");
+        }
+        requestDispatcher.forward(request,response);
     }
 }
--- src/main/java/com/proyecto/administraciontiendawebapp/model/data/dao/ClienteDAO.java
+++ src/main/java/com/proyecto/administraciontiendawebapp/model/data/dao/ClienteDAO.java
@@ -14,25 +14,33 @@ import static org.jooq.impl.DSL.name;
 import static org.jooq.impl.DSL.table;
 
 public class ClienteDAO {
-    public static void registarCliente(DSLContext query, Cliente cliente){
-        Table tablaCliente= table(name("Cliente"));
-        Field[] columnas = tablaCliente.fields("rut","nombre","direccion","telefono");
-        query.insertInto(tablaCliente, columnas[0], columnas[1],columnas[2],columnas[3])
-                .values(cliente.getRut(),cliente.getNombre(),cliente.getDireccion(),cliente.getTelefono())
-                .execute();
+    public static boolean registarCliente(DSLContext query, Cliente cliente) {
+        Table tablaCliente = table(name("cliente"));
+        Field[] columnas = tablaCliente.fields("rut_cliente", "nombre_cliente", "direccion", "telefono", "correo_electronico");
+        int result = 0;
+        try {
+            result=query.insertInto(tablaCliente, columnas[0], columnas[1], columnas[2], columnas[3], columnas[4])
+                    .values(cliente.getRut(), cliente.getNombre(), cliente.getDireccion(), cliente.getTelefono(), cliente.getCorreo())
+                    .execute();
+        } catch (Exception e) {
+            System.out.println("Rut Ya registrado!!");
+        }
+
+        return result==1;
     }
     public static List obtenerCliente(DSLContext query, String columnaTabla, Object dato){
-        Result resultados = query.select().from(DSL.table("Cliente")).where(DSL.field(columnaTabla).eq(dato)).fetch();
+        Result resultados = query.select().from(DSL.table("cliente")).where(DSL.field(columnaTabla).eq(dato)).fetch();
         return obtenerListaClientes(resultados);
     }
     private static List obtenerListaClientes(Result resultados){
         List<Cliente> clientes= new ArrayList<>();
         for(int fila=0; fila<resultados.size();fila++){
-            String rut = (String) resultados.getValue(fila,"rut");
-            String nombre = (String) resultados.getValue(fila,"nombre");
+            String rut = (String) resultados.getValue(fila,"rut_cliente");
+            String nombre = (String) resultados.getValue(fila,"nombre_cliente");
             String direccion = (String) resultados.getValue(fila,"direccion");
             String telefono = (String) resultados.getValue(fila,"telefono");
-            clientes.add(new Cliente(rut,nombre,direccion,telefono));
+            String correo= (String) resultados.getValue(fila,"correo_electronico");
+            clientes.add(new Cliente(rut,nombre,direccion,telefono,correo));
         }
         return clientes;
     }
